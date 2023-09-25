f0af9dd051fa1ff4da81d48734f1ea1ee325e2ea
ghost75046 <ghost75046@yandex.ru>
2023-09-25 17:23:54 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css'
+import logo from './defaultEgg.png'
+import logo2 from './vino07.jpg'
+import shava from './shaverma.png'
+import crispsCrunch from './crispsCrunch.mp3'
+
+function App(props) {
+
+
+    const onClickHandler = () => {
+        console.log('test')
+
+
+    }
+    const playMusic = () => {
+        let music = new Audio(crispsCrunch)
+        music.play()
+    }
+
+    const handleClick= () =>{
+        onClickHandler();playMusic();
+    }
+    return (
+        <div className='mainDiv'>
+
+            <div>
+                комната
+                <div className='hungryLevel'>
+                    <div className='hungryItem'>
+                        <img src={shava} alt="#"/>
+                    </div>
+                    <div className='hungryItem'>
+                        <img src={shava} alt="#"/>
+                    </div>
+                    <div className='hungryItem'>
+                        <img src={shava} alt="#"/>
+                    </div>
+                </div>
+                <div className='myEgg'>
+                    {/*<img src={logo} alt="#" className='myEgg'/>*/}
+
+                    <button onClick={onClickHandler}>1</button>
+                    {/*<audio src={Bethoven}></audio>*/}
+
+                    <button onClick={handleClick}>2</button>
+                    {/*<img src={logo} alt="#"/>*/}
+                    <button>3</button>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    )
+
+}
+
+
+export default App
---
+++ src/MySql.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1,login:'support',password:'elephant'},
+    {id:2,login:'admin',password:'sql'}
+]
+
+
+
+export default users
